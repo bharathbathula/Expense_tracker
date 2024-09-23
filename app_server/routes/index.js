5164914f4b85be4b132262342410639b24fadb8e
bharathbathula <119663404+bharathbathula@users.noreply.github.com>
2024-09-23 23:10:11 +0530

    Add files via upload

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const ctrlLocations = require("../controllers/locations");
 const ctrlOthers = require("../controllers/others");
+
 /* Locations pages */
 router.get("/", ctrlLocations.homepage);
 router.get("/transactions", ctrlLocations.transactionInfo);
 router.get("/transactions/addexpense", ctrlLocations.addExpense);
+router.post("/expenses", ctrlLocations.createExpense); // Add this route to handle form submission for adding an expense
+
 /* Other pages */
 router.get("/about", ctrlOthers.about);
+
 module.exports = router;
