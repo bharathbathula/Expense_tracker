e49542ec6bdcaa53ef8a2ff16761d492fe9e446b
bharathbathula <119663404+bharathbathula@users.noreply.github.com>
2024-09-23 23:06:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose"); // Add Mongoose for MongoDB
 
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
@@ -19,6 +20,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://bharath:123@cluster0.olt8a.mongodb.net/expense_tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// MongoDB Connection Event Listeners
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+// Use Routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
@@ -29,11 +49,11 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render("error");
 });
